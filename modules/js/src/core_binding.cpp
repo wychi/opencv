7c169a1eea505371a3cb398e77c28ff0096b1147
CJKu <cku@mozilla.com>
2015-03-31 18:37:19 +0800

    TDD: Try to make test_matrix.js pass

--- modules/js/src/core_binding.cpp
+++ modules/js/src/core_binding.cpp
@@ -1,17 +1,185 @@
-
 #include "opencv2/core/mat.hpp"
 #include <emscripten/bind.h>
-/* Embinding code */
 
 using namespace emscripten;
-EMSCRIPTEN_BINDINGS(matrix) {
+
+// Mat wrappers
+// The 1st parameter of a free getter need to be const reference
+//   struct GetterPolicy<GetterReturnType (*)(const GetterThisType&)>
+// Member getters have to be const function.
+//   struct GetterPolicy<GetterReturnType (GetterThisType::*)() const>
+static uintptr_t Mat_getData(const cv::Mat& mat)
+{
+  return uintptr_t(mat.data);
+}
+
+static void Mat_setData(cv::Mat& mat, uintptr_t data)
+{
+  mat.data = reinterpret_cast<uchar *>(data);
+}
+
+static std::vector<int> Mat_getSize(const cv::Mat &mat)
+{
+  std::vector<int> size;
+  for (int i = 0; i < mat.dims; i++) {
+    size.push_back(mat.size[i]);
+  }
+
+  return size;
+}
+
+// http://emscripten-discuss.narkive.com/51XSL2Cs/returning-references-with-embind
+uintptr_t Mat_at(cv::Mat &mat, int i0, int i1)
+{
+  return (uintptr_t)&mat.at<unsigned char>(i0, i1);
+}
+
+uintptr_t Mat_ptr(cv::Mat &mat, int i0)
+{
+  return (uintptr_t)mat.ptr(i0);
+}
+
+uintptr_t Mat_ptr(cv::Mat &mat, int i0, int i1)
+{
+  return (uintptr_t)mat.ptr(i0, i1);
+}
+
+template <int N, typename R>
+struct FunctionWrapper;
+
+template <int N, typename R, typename P1, typename P2>
+struct FunctionWrapper<N, R (*)(P1, P2)>
+{
+  typedef R (*FunctionSig)(P1, P2);
+
+  static void bind(FunctionSig f) { mF = f;}
+  template <typename T> static T call(P1 p1, P2 p2)
+  { return mF(p1, p2); }
+
+  static FunctionSig mF;
+};
+
+template <int N, typename R, typename P1, typename P2>
+typename FunctionWrapper<N, R (*)(P1, P2)>::FunctionSig
+FunctionWrapper<N, R (*)(P1, P2)>::mF = nullptr;
+
+template <int N, typename R, typename P1, typename P2, typename P3>
+struct FunctionWrapper<N, R (*)(P1, P2, P3)>
+{
+  typedef R (*FunctionSig)(P1, P2, P3);
+
+  static void bind(FunctionSig f) { mF = f;}
+  template <typename T> static T call(P1 p1, P2 p2, P3 p3)
+  { return mF(p1, p2, p3); }
+
+  static FunctionSig mF;
+};
+
+template <int N, typename R, typename P1, typename P2, typename P3>
+typename FunctionWrapper<N, R (*)(P1, P2, P3)>::FunctionSig
+FunctionWrapper<N, R (*)(P1, P2, P3)>::mF = nullptr;
+
+// TODO:
+// How to auto gen wrapper for function with void * pointer parameters.
+cv::Mat *createMat(cv::Size size, int type, uintptr_t data, size_t step) {
+  return new cv::Mat(size, type, reinterpret_cast<void *>(data), step);
+}
+
+EMSCRIPTEN_BINDINGS(stl_wrappers) {
+  register_vector<int>("VectorInt");
+}
+
+EMSCRIPTEN_BINDINGS(ocv_matrix) {
+  typedef FunctionWrapper<0, cv::MatExpr (*)(int, int, int)> MatZero1;
+  typedef FunctionWrapper<1, cv::MatExpr (*)(int, int, int)> MatEye1;
+  typedef FunctionWrapper<2, cv::MatExpr (*)(int, int, int)> MatOnes1;
+  MatZero1::bind(cv::Mat::zeros);
+  MatEye1::bind(cv::Mat::eye);
+  MatOnes1::bind(cv::Mat::ones);
+  typedef FunctionWrapper<0, cv::MatExpr (*)(cv::Size, int)> MatZero2;
+  typedef FunctionWrapper<1, cv::MatExpr (*)(cv::Size, int)> MatEye2;
+  typedef FunctionWrapper<2, cv::MatExpr (*)(cv::Size, int)> MatOnes2;
+  MatZero2::bind(cv::Mat::zeros);
+  MatEye2::bind(cv::Mat::eye);
+  MatOnes2::bind(cv::Mat::ones);
+
+  value_array<cv::Size>("Size")
+    .element(&cv::Size::height)
+    .element(&cv::Size::width)
+    ;
   class_<cv::Mat>("Mat")
+    // Constructor && external creators.
+    .constructor<>()
     .constructor<int, int, int>()
+    .constructor<const cv::Mat&>()
+    .constructor(&createMat, allow_raw_pointers())
+    // Mat properties
     .function("type", &cv::Mat::type)
     .function("depth", &cv::Mat::depth)
     .function("channels", &cv::Mat::channels)
     .function("empty", &cv::Mat::empty)
+    .function("size", &Mat_getSize)
+    // data access
+    .function("row", &cv::Mat::row)
+    .function("col", &cv::Mat::row)
+    .property("data", &Mat_getData, &Mat_setData)
+    // We support Mat::ptr and do not supprt Mat::at.
+    // User should use typed array to access element data with correct type.
+    .function("ptr", select_overload<uintptr_t (cv::Mat &, int)>(&Mat_ptr))
+    .function("ptr", select_overload<uintptr_t (cv::Mat &, int, int)>(&Mat_ptr))
+    .function("create", select_overload<void (int, int, int)>(&cv::Mat::create))
+    .function("create", select_overload<void (cv::Size, int)>(&cv::Mat::create))
+    // Since we can't overwrite + operator at js side, we should return a matrix
+    // instance, not MatExpr one, in exported eyes/ones/zeros
+    .class_function("zeros", &MatZero1::call<cv::Mat>)
+    .class_function("ones", &MatOnes1::call<cv::Mat>)
+    .class_function("eye", &MatEye1::call<cv::Mat>)
+    .class_function("zeros", &MatZero2::call<cv::Mat>)
+    .class_function("ones", &MatOnes2::call<cv::Mat>)
+    .class_function("eye", &MatEye2::call<cv::Mat>)
+/*
+    Mat rowRange(int startrow, int endrow) const;
+    Mat rowRange(const Range& r) const;
+    Mat colRange(int startcol, int endcol) const;
+    Mat colRange(const Range& r) const;
+    static Mat diag(const Mat& d);
+
+    Mat clone() const;
+    void copyTo( OutputArray m ) const;
+    void copyTo( OutputArray m, InputArray mask ) const;
+    void convertTo( OutputArray m, int rtype, double alpha=1, double beta=0 ) const;
+    void assignTo( Mat& m, int type=-1 ) const;
+    Mat& operator = (const Scalar& s);
+    Mat& setTo(InputArray value, InputArray mask=noArray());
+    Mat reshape(int cn, int rows=0) const;
+    Mat reshape(int cn, int newndims, const int* newsz) const;
+    MatExpr t() const;
+    MatExpr inv(int method=DECOMP_LU) const;
+    MatExpr mul(InputArray m, double scale=1) const;
+    Mat cross(InputArray m) const;
+    double dot(InputArray m) const;
+    static MatExpr zeros(int ndims, const int* sz, int type);
+    static MatExpr ones(int ndims, const int* sz, int type);
+    void deallocate();
+    void copySize(const Mat& m);
+    void reserve(size_t sz);
+    void resize(size_t sz);
+    void resize(size_t sz, const Scalar& s);
+    void push_back_(const void* elem);
+    template<typename _Tp> void push_back(const _Tp& elem);
+    template<typename _Tp> void push_back(const Mat_<_Tp>& elem);
+    void push_back(const Mat& m);
+    void pop_back(size_t nelems=1);
+    void locateROI( Size& wholeSize, Point& ofs ) const;
+    Mat& adjustROI( int dtop, int dbottom, int dleft, int dright );
+    Mat operator()( Range rowRange, Range colRange ) const;
+    Mat operator()( const Rect& roi ) const;
+    Mat operator()( const Range* ranges ) const;
+    Mat diag(int d=0) const;
+*/
     ;
+
+    // constants.
     constant("CV_8UC1", CV_8UC1);
     constant("CV_8UC2", CV_8UC2);
     constant("CV_8UC3", CV_8UC3);
@@ -41,4 +209,12 @@ EMSCRIPTEN_BINDINGS(matrix) {
     constant("CV_64FC2", CV_64FC2);
     constant("CV_64FC3", CV_64FC3);
     constant("CV_64FC4", CV_64FC4);
+    constant("CV_8U", CV_8U);
+    constant("CV_8S", CV_8S);
+    constant("CV_16U", CV_16U);
+    constant("CV_16S", CV_16S);
+    constant("CV_32S", CV_32S);
+    constant("CV_32F", CV_32F);
+    constant("CV_64F", CV_64F);
+    constant("AUTO_STEP", size_t(cv::Mat::AUTO_STEP));
 }
--- modules/js/test/test_mat.js
+++ modules/js/test/test_mat.js
@@ -1,62 +1,95 @@
 // Test all classes defined in mat.hpp
-
+//
 QUnit.test("test_mat_creation", function(assert) {
   // Mat constructors.
+  // Mat::Mat(int rows, int cols, int type)
   {
-    // Mat::Mat(int rows, int cols, int type)
-    //  : by giving size and type
     let mat = new Module.Mat(10, 20, Module.CV_8UC3);
+
     assert.equal(mat.type(), Module.CV_8UC3);
     assert.equal(mat.depth(), Module.CV_8U);
     assert.equal(mat.channels(), 3);
     assert.ok(mat.empty() === false);
-    assert.equal(mat.size().width === 10);
-    assert.equal(mat.size().height === 20);
+
+    let size = mat.size();
+    assert.ok(size.size() === 2);
+    assert.equal(size.get(0), 10);
+    assert.equal(size.get(1), 20);
 
     mat.delete();
   }
 
+  // Mat::Mat(const Mat &)
   {
-    // Mat::Mat(const Mat &)
     //  : Copy from another Mat
-    let mat = new Module.Mat(mat);
-    assert.equal(mat.type(), mat.type());
-    assert.equal(mat.depth(), mat.depth());
-    assert.equal(mat.channels(), mat.channels());
-    assert.equal(mat.empty(), mat.empty());
-    assert.equal(mat.size(), mat.size());
+    let mat1 = new Module.Mat(10, 20, Module.CV_8UC3);
+    let mat2 = new Module.Mat(mat1);
 
-    mat.delete();
+    assert.equal(mat2.type(), mat1.type());
+    assert.equal(mat2.depth(), mat1.depth());
+    assert.equal(mat2.channels(), mat1.channels());
+    assert.equal(mat2.empty(), mat1.empty());
+
+    let size1 = mat1.size();
+    let size2 = mat2.size();
+    assert.ok(size1.size() === size2.size());
+    assert.ok(size1.get(0) === size2.get(0));
+    assert.ok(size1.get(1) === size2.get(1));
+
+    mat1.delete();
+    mat2.delete();
   }
 
+  // Mat::Mat(Size size, int type, void *data, size_t step=AUTO_STEP)
   {
-    // Mat::Mat(Size size, int type, void *data, size_t step=AUTO_STEP)
-    //  : primitive pointer is not supported. Need to create an external constructor.
-    let matSize = {
-      width : 10,
-      height: 10
-    }
     // 10 * 10 and one channel
     let data = Module._malloc(10 * 10 * 1);
-    let view = new Uint8Array(Module.HEAPU8.buffer, data, 10 * 10 * 1);
-    let mat = new Module.Mat(matSize, Module.CV_8UC1, view.byteOffset, Module.AUTO_STEP);
+    let mat = new Module.Mat([10, 10], Module.CV_8UC1, data, Module.AUTO_STEP);
+
     assert.equal(mat.type(), Module.CV_8UC1);
     assert.equal(mat.depth(), Module.CV_8U);
     assert.equal(mat.channels(), 1);
     assert.ok(mat.empty() === false);
-    assert.equal(mat.size().width === 10);
-    assert.equal(mat.size().height === 10);
 
-    data.delete();
+    let size = mat.size();
+    assert.ok(size.size() === 2);
+    assert.ok(size.get(0) === 10);
+    assert.ok(size.get(1) === 10);
+
+    mat.delete();
   }
 });
 
-QUnit.test("test_mat_buffer_allocation", function(assert) {
-  // Allocate buffer after Mat creation.
-  //   Mat::create
+QUnit.test("test_mat_create", function(assert) {
+  //  Mat::create(int, int, int)
+  {
+    let mat = new Module.Mat();
+    mat.create(10, 5, Module.CV_8UC3);
+    let size = mat.size();
+
+    assert.ok(mat.type() === Module.CV_8UC3);
+    assert.ok(size.get(0) === 10);
+    assert.ok(size.get(1) === 5);
+    assert.ok(mat.channels() === 3);
+
+    mat.delete();
+  }
+  //  Mat::create(Size, int)
+  {
+    let mat = new Module.Mat();
+    mat.create([10, 5], Module.CV_8UC4);
+    let size = mat.size();
+
+    assert.ok(mat.type() === Module.CV_8UC4);
+    assert.ok(size.get(0) === 10);
+    assert.ok(size.get(1) === 5);
+    assert.ok(mat.channels() === 4);
+
+    mat.delete();
+  }
 });
 
-QUnit.test("test_mat_matrix_expression", function(assert) {
+QUnit.skip("test_mat_matrix_expression", function(assert) {
   // Matrix operations: A and B are Matrix. S is scalar
   //  A + B
   //  A * C
@@ -65,120 +98,172 @@ QUnit.test("test_mat_matrix_expression", function(assert) {
   // How about replace "+" by add function, "-" by sub and so on.
 });
 
-QUnit.test("test_mat_elementAccess", function(assert) {
-  const RValue = 1;
+QUnit.test("test_mat_ptr", function(assert) {
+  const RValue = 3;
   const GValue = 7;
   const BValue = 197;
-  const AValue = 123;
 
-  // Test 1.
-  //   Mat::data
+  // Module.CV_8UC1 + Mat::ptr(int).
   {
-    let mat = new Module.Mat(10, 10, Module.CV_8UC4);
-    let dataView = Module.HEAPU8.subarray(mat.data);
+    let mat = new Module.Mat(10, 10, Module.CV_8UC1);
+    let view = Module.HEAPU8.subarray(mat.data);
 
-    // Alter Mat::data directly.
-    dataView[0] = RValue;
-    dataView[1] = GValue;
-    dataView[2] = BValue;
-    dataView[3] = AValue;
+    // Alter matrix[2, 1].
+    let step = 10;
+    view[2 * step + 1] = RValue;
 
-    // Read altered data by Mat::at and verify.
-    dataView = Module.HEAPU8.subarray(mat.at(0, 0));
+    // Access matrix[2, 1].
+    view = Module.HEAPU8.subarray(mat.ptr(2));
 
-    assert.equal(dataView[0], RValue);
-    assert.equal(dataView[1], GValue);
-    assert.equal(dataView[2], BValue);
-    assert.equal(dataView[3], AValue);
+    assert.equal(view[1], RValue);
   }
 
-  // No good. Template is not supported in js.
-  //  at<uchar> => atInteger
-  //  at<Double> => atDouble
-  //  at<Vect3b> => atVec3b
-  //  at<Point> ?? Are we going to export Point?
-
-  // Test 2.
-  //   Mat::at<uchar>
-  {
-    const GrayScaleValue = 7;
-
-    // Write the value of a specific pixel.
-    let mat = new Module.Mat(1, 10, Module.CV_8UC1);
-    var pixel = Module.HEAPU8.subarray(mat.at(5));
-
-    pixel = GrayScaleValue;
-
-    // Verify.
-    pixel = Module.HEAPU8.subarray(mat.at(5));
-    assert.equal(pixel[0], GrayScaleValue);
-  }
-
-  // Test 3.
-  //   Mat::at<Vect3b>
+  // Module.CV_8UC3 + Mat::ptr(int).
   {
     let mat = new Module.Mat(10, 10, Module.CV_8UC3);
-    // In c++, you need only call Mat::at<type>(index), which does two
-    // things for you
-    //   1. pointer address evaluation by index.
-    //   2. type casting by template argument.
-    // In javascript, you have to
-    //   1. call Matrix.at to get the raw address.
-    //   2. create a data viewer for this returning address.
-    // Sorta verbose.
-    // We may either inherit Mat in JS side to combine #1 and #2 in
-    // one function call, or use emscripten::val in c++ wrap function.
-    var pixel = Module.HEAPU8.subarray(mat.at(5, 5));
+    let view = Module.HEAPU8.subarray(mat.data);
 
-    pixel[0] = RValue;
-    pixel[1] = GValue;
-    pixel[2] = BValue;
-    pixel[3] = AValue;
+    // Alter matrix[2, 1].
+    let step = 3 * 10;
+    view[2 * step + 3] = RValue;
+    view[2 * step + 3 + 1] = GValue;
+    view[2 * step + 3 + 2] = BValue;
 
-    pixel = new Uint8Array(Module.HEAPU8, mat.at(5, 5), 3);
+    // Access matrix[2, 1].
+    view = Module.HEAPU8.subarray(mat.ptr(2));
 
-    assert.equal(pixel[0], RValue);
-    assert.equal(pixel[1], GValue);
-    assert.equal(pixel[2], BValue);
-    assert.equal(pixel[3], AValue);
+    assert.equal(view[3], RValue);
+    assert.equal(view[3 + 1], GValue);
+    assert.equal(view[3 + 2], BValue);
+  }
 
-    mat.delete();
+  // Module.CV_8UC3 + Mat::ptr(int, int).
+  {
+    let mat = new Module.Mat(10, 10, Module.CV_8UC3);
+    let view = Module.HEAPU8.subarray(mat.data);
+
+    // Alter matrix[2, 1].
+    let step = 3 * 10;
+    view[2 * step + 3] = RValue;
+    view[2 * step + 3 + 1] = GValue;
+    view[2 * step + 3 + 2] = BValue;
+
+    // Access matrix[2, 1].
+    view = Module.HEAPU8.subarray(mat.ptr(2, 1));
+
+    assert.equal(view[0], RValue);
+    assert.equal(view[1], GValue);
+    assert.equal(view[2], BValue);
   }
 });
 
 QUnit.test("test_mat_zeros", function(assert) {
-  var mat = Module.Mat.zeros(10, 10, Module.CV_8UC1);
-  var total = 0;
-  Module.HEAPU8.subarray((0).forEach(function(element, index, array) {
-    total += element;
-  });
+  // Mat::zero(int, int, int)
+  {
+    let mat = Module.Mat.zeros(10, 10, Module.CV_8UC1);
+    let view = Module.HEAPU8.subarray(mat.data);
 
-  assert.equal(total, 0);
+    // TBD
+    // Figurr out why array.prototype is undefined. Since that's undifined, I can't
+    // use any array member function, such as every/forEach.
+    //assert.ok(view.every(function(x) { return x === 0; }));
+
+    var total = 0;
+    for (let i = 0; i < 100; i++) {
+      total += view[i];
+    }
+
+    assert.ok(total === 0);
+  }
+  // Mat::zero(Size, int)
+  {
+    let mat = Module.Mat.zeros([10, 10], Module.CV_8UC1);
+    let view = Module.HEAPU8.subarray(mat.data);
+
+    var total = 0;
+    for (let i = 0; i < 100; i++) {
+      total += view[i];
+    }
+
+    assert.ok(total === 0);
+  }
 });
 
 QUnit.test("test_mat_ones", function(assert) {
-  var mat = Module.Mat.ones(10, 10, Module.CV_8UC1);
-  var total = 0;
-  Module.HEAPU8.subarray((0).forEach(function(element, index, array) {
-    total += element;
-  });
+  // Mat::ones(int, int, int)
+  {
+    var mat = Module.Mat.ones(10, 10, Module.CV_8UC1);
+    var view = Module.HEAPU8.subarray(mat.data);
 
-  assert.equal(total, 100);
+    var total = 0;
+    for (let i = 0; i < 100; i++) {
+      total += view[i];
+    }
+
+    assert.ok(total === 100);
+  }
+
+  // Mat::ones(Size, int)
+  {
+    var mat = Module.Mat.ones([10, 10], Module.CV_8UC1);
+    var view = Module.HEAPU8.subarray(mat.data);
+
+    var total = 0;
+    for (let i = 0; i < 100; i++) {
+      total += view[i];
+    }
+
+    assert.ok(total === 100);
+  }
 });
 
-QUnit.test("test_mat_eyes", function(assert) {
-  var mat = Module.Mat.eyes(10, 10, Module.CV_8UC1);
-  var total = 0;
-  Module.HEAPU8.subarray((0).forEach(function(element, index, array) {
-    total += element;
-  });
+QUnit.test("test_mat_eye", function(assert) {
+  // Mat::eye(int, int, int)
+  {
+    var mat = Module.Mat.eye(10, 10, Module.CV_8UC1);
+    var view = Module.HEAPU8.subarray(mat.data);
 
-  assert.equal(total, 10);
+    var total = 0;
+    for (let i = 0; i < 100; i++) {
+      total += view[i];
+    }
+
+    assert.ok(total === 10);
+  }
+
+  // Mat::eye(Size, int)
+  {
+    var mat = Module.Mat.eye([10, 10], Module.CV_8UC1);
+    var view = Module.HEAPU8.subarray(mat.data);
+
+    var total = 0;
+    for (let i = 0; i < 100; i++) {
+      total += view[i];
+    }
+
+    assert.ok(total === 10);
+  }
 });
 
-QUnit.test("test_mat_properties", function(assert) {
-  // TBD: do we need this test? test_mat_creation containts most of getter we
-  // need.
+QUnit.skip("test_mat_miscs", function(assert) {
+  var mat = Module.Mat.ones(5, 5, Module.CV_8UC2);
+  {
+    let col = mat.col(1);
+    let data = Module.HEAPU8.subarray(col.data);
+    assert.equal(data[0], 1);
+    assert.equal(data[4], 1);
+
+    col.delete();
+  }
+
+  {
+    let row = mat.row(1);
+    let data = Module.HEAPU8.subarray(row.data);
+    assert.equal(data[0], 1);
+    assert.equal(data[4], 1);
+
+    row.delete();
+  }
 });
 
 
