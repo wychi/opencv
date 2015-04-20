345e2a9fe59b90af85704529fe76fa3a32388fd7
CJKu <cku@mozilla.com>
2015-04-21 02:28:18 +0800

    threshold sample

--- samples/js/js/opencv_demo.js
+++ samples/js/js/opencv_demo.js
@@ -9,17 +9,30 @@ OpenCV.ImageGenerator = {
   _ctx: null,
 
   load: function(blob) {
-    this._lazyInit();
+    /*this._lazyInit();
 
+    let $image = $('#baboon_img');
+    this._canvas.width = $image[0].width;
+    this._canvas.height = $image[0].height;
+
+    this._ctx.drawImage($image[0], 0, 0);
+    return new Promise(function(resolve, reject) {
+        resolve();
+      });*/
+    this._lazyInit();
     var self = this;
+    var reader = new FileReader();
     var promise = new Promise(function(resolve, reject) {
-      var reader = new FileReader();
-      reader.onload = function()  {
+      reader.onload = function(e)  {
         var dataURL = reader.result;
+        //var dataURL = reader.result.split(',')[1];
+        //dataURL = atob(dataURL);
+        //var aaa = btoa(decodeURIComponent(escape(dataURL)));
 
         try {
           self
             ._drawImage(dataURL)
+            //._drawImage("data:image/png;base64," + aaa)
             .then(function() { 
               resolve();
             });
@@ -46,22 +59,23 @@ OpenCV.ImageGenerator = {
   },
 
   _drawImage: function SI_load(dataURL) {
-    var self = this;
-
     var $image = $("<img>");
     $image.appendTo("body");
 
+    $image.attr('display', 'none');
+    $image.attr('id', 'draw_target');
+    $image.attr('src', dataURL);
+
+    var self = this;
     return new Promise(function(resolve, reject) {
       $image[0].onload = function() {
         self._canvas.width = this.width;
         self._canvas.height = this.height;
 
         self._ctx.drawImage(this, 0, 0);
-        $('body').remove('#draw_target');
+        $('#draw_target').remove();
         resolve();
       }
-      $image.attr('id', 'draw_target');
-      $image.attr('src', dataURL);
     });
   },
 
@@ -102,10 +116,33 @@ $(function() {
       OpenCV.ImageGenerator
         .load(file)
         .then(function() {
+          var imageData = OpenCV.ImageGenerator.createImageData();
+          // Draw source image
           var $canvas = $("<canvas>");
+          var ctx = $canvas[0].getContext('2d');
           $canvas.appendTo('body');
-          var data = OpenCV.ImageGenerator.createImageData();
-          $canvas[0].putImageData(data.data, 0, 0);
+          $canvas.attr('width', imageData.width);
+          $canvas.attr('height', imageData.height);
+          ctx.putImageData(imageData, 0, 0);
+
+          // Test threshold 
+          let mat = new Module.Mat(imageData.height, imageData.width, Module.CV_8UC4);
+          let matView = Module.HEAPU8.subarray(mat.data);
+          matView.set(imageData.data);
+
+          const THRESHOLD = 100.0;
+          const THRESHOLD_MAX = 210.0;
+          let dest = new Module.Mat();
+          Module.threshold(mat, dest, THRESHOLD, THRESHOLD_MAX, Module.THRESH_BINARY);
+          let dataLen = dest.size().get(0) * dest.size().get(1) * dest.elemSize();
+          let destView = Module.HEAPU8.subarray(dest.data, dest.data + dataLen);
+          imageData.data.set(destView);
+          var $canvas2 = $("<canvas>");
+          ctx = $canvas2[0].getContext('2d');
+          $canvas2.appendTo('body');
+          $canvas2.attr('width', imageData.width);
+          $canvas2.attr('height', imageData.height);
+          ctx.putImageData(imageData, 0, 0);
         })
         ;
     })
